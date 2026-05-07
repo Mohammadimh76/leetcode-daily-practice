8e88a6021e644303a3bbc7f96459fe69e8195602
Mohammad Hossein Mohammadi <m.h_mohammadi2015@yahoo.com>
2026-05-07 14:06:02 +0330

    python folder
    this folder for share solution problems.

---
+++ python/0344-Reverse String/solution.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def reverseString(s: List[str]) -> None:
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+
+        print (s)
+
+Solution.reverseString(["h","e","l","l","o"])
